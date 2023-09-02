4a8592f56b950e0cdfeb97eb08ca6c75684b664c
takumi-hash <39505885+takumi-hash@users.noreply.github.com>
2023-09-02 20:04:37 +0900

    minor fix. prepartion done.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,4 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
-const path = require("path")
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -9,7 +7,6 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/postcss",
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -18,23 +15,5 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
-  staticDirs:['public'],
-  babel: async options => ({
-    ...options,
-    plugins: [
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-proposal-private-methods",
-      "@babel/plugin-proposal-private-propery-in-object",
-    ],
-  }),
-  webpackFinal: async(config) => {
-    config.resolve.plugins = [
-      new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, "../tsconfig.json")
-      }),
-    ];
-    return config
-  }
 };
-
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,57 +1,15 @@
-import type { Preview, Decorator } from "@storybook/react";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import { withThemes } from '@react-theming/storybook-addon';
-import {theme} from "../src/themes"
-import * as NextImage from "next/image"
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
-
-export const GlobalStyle = createGlobalStyle`
-  html,
-  body,
-  textarea {
-    padding: 0;
-    margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-
-  a {
-    cursor: pointer;
-    text-decoration: none;
-    transition: .25s;
-    color: #000;
-  }
-`;
-
-const providerFn = ({ story }) => {
-  return (
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        {story()}
-      </ThemeProvider>
-  );
-};
-
-const themingDecorator = withThemes(null, [theme], { providerFn });
-
-export const decorators= [themingDecorator,];
+import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
-  parameters: parameters,
-  decorators: decorators
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
 };
 
 export default preview;
