2b65409e04a023dd9abf79ee1b2df42b0a0d2735
takumi-hash <39505885+takumi-hash@users.noreply.github.com>
2023-09-03 14:43:31 +0900

    Fix api proxy source config

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
     return [
       {
         // ex. /api/proxy
-        source: `${process.env.API_BASE_URL}/:match*`,
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
         // ex. http://localhost:8000
         destination: `${process.env.API_BASE_URL}/:match*`,
       },
